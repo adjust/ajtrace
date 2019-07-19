42db52219bc9fe143c805c2c02f46687ff5a09ee
Ildar Musin <zildermann@gmail.com>
2019-07-19 15:47:04 +0200

    Add setup.py and modified files layout in order to be able to build a distribution

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ajtrace',
+    version='0.1dev',
+    packages=['ajtrace', 'ajtrace.ebpf_modules'],
+    url='https://github.com/adjust/ajtrace',
+    author='Ildar Musin'
+)
