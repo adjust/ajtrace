53b5795f621d0d9017d75b669d69b947e2a4f495
Ildar Musin <zildermann@gmail.com>
2019-07-23 11:13:20 +0200

    Get rid of deprication warning by explicitly specifying Loader for yaml.load()

--- ajtrace/settings.py
+++ ajtrace/settings.py
@@ -4,7 +4,7 @@ class GlobalConfig:
     __config = None
 
     def initialize(path):
-        GlobalConfig.__config = yaml.load(open(path, 'r'))
+        GlobalConfig.__config = yaml.load(open(path, 'r'), Loader=yaml.FullLoader)
 
     def get(key, default=None):
         return GlobalConfig.__config.get(key, default)
